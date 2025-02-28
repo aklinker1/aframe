94d0d08dd505f4c5adb60820ba6a77aa0b2e7c4a
Aaron <aaronklinker1@gmail.com>
2025-02-28 10:38:46 -0600

    feat!: Make  a function that accepts options, allowing overriding specific file responses

--- src/client/server.ts
+++ src/client/server.ts
@@ -11,53 +11,67 @@ export interface AframeServer {
 /**
  * Fetches a file from the `public` directory.
  */
-export async function fetchStatic(request: Request): Promise<Response> {
-  const path = new URL(request.url).pathname.replace(/\/+$/, "");
+export function fetchStatic(options?: {
+  /** Override the fetch behavior of a file. */
+  onFetch?: (
+    path: string,
+    file: BunFile,
+  ) => Promise<Response | undefined> | Response | undefined;
+}): (request: Request) => Promise<Response> {
+  return async (request) => {
+    const path = new URL(request.url).pathname.replace(/\/+$/, "");
 
-  const paths = [
-    `${import.meta.publicDir}${path}`,
-    `${import.meta.publicDir}${path}/index.html`,
-  ];
+    const paths = [
+      `${import.meta.publicDir}${path}`,
+      `${import.meta.publicDir}${path}/index.html`,
+    ];
 
-  // Only fallback on the root HTML file when building application
-  if (import.meta.command === "build") {
-    paths.push(`${import.meta.publicDir}/index.html`);
-  }
+    // Only fallback on the root HTML file when building application
+    if (import.meta.command === "build") {
+      paths.push(`${import.meta.publicDir}/index.html`);
+    }
 
-  for (const path of paths) {
-    const gzFile = Bun.file(path + ".gz");
-    const file = Bun.file(path);
+    for (const path of paths) {
+      const isHtml = path.includes(".html");
+      const gzFile = Bun.file(path + ".gz");
+      const file = Bun.file(path);
 
-    if (await isFile(gzFile)) {
-      return new Response(gzFile.stream(), {
+      if (await isFile(gzFile)) {
+        const customResponse = await options?.onFetch?.(path, file);
+        if (customResponse) return customResponse;
+        return new Response(gzFile.stream(), {
+          headers: {
+            ...(isHtml ? {} : headers),
+            "content-type": file.type,
+            "content-encoding": "gzip",
+          },
+        });
+      }
+
+      if (await isFile(file)) {
+        const customResponse = await options?.onFetch?.(path, file);
+        if (customResponse) return customResponse;
+
+        return new Response(file.stream(), { headers });
+      }
+    }
+
+    return new Response(
+      `<html>
+        <body>
+          This is a placeholder for your root <code>index.html</code> file during development.
+          <br/>
+          In production (or via the app's dev server), this path will fallback on the root <code>index.html</code>.
+        </body>
+      </html>`,
+      {
         headers: {
+          "Content-Type": "text/html",
           ...headers,
-          "content-type": file.type,
-          "content-encoding": "gzip",
         },
-      });
-    }
-
-    if (await isFile(file)) {
-      return new Response(file.stream(), { headers });
-    }
-  }
-
-  return new Response(
-    `<html>
-      <body>
-        This is a placeholder for your root <code>index.html</code> file during development.
-        <br/>
-        In production (or via the app's dev server), this path will fallback on the root <code>index.html</code>.
-      </body>
-    </html>`,
-    {
-      headers: {
-        "Content-Type": "text/html",
-        ...headers,
       },
-    },
-  );
+    );
+  };
 }
 
 async function isFile(file: BunFile): Promise<boolean> {
